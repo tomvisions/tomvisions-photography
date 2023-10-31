82868b23136e277457fe553f6d369517c1fdad8d
Tom <tomc@tomvisions.com>
2023-10-31 13:54:22 -0400

    adding image service and updating layout

---
+++ src/app/image.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+const Buffer = require('buffer').Buffer;
+declare var require: any;
+//declare const Buffer:any;
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageService {
+
+  private _PARAM_LOCATION:string|null = 'tomvisions/main-images';
+  private _PARAM_FRONTCLOUD:string|null = 'https://d34wc8uzk8vrsx.cloudfront.net';
+
+  constructor() { }
+
+  loadImage983x409(image: string) {
+    const resizedImage = this.resizeWithInS3(image, {
+      "resize": {
+        "width": 983,
+        "height": 409,
+        "fit": "cover"
+      }
+    });
+
+    return `${this._PARAM_FRONTCLOUD}/${resizedImage}`;
+  }
+
+
+  /**
+   * Setup Signature so that a specific bucket and key are resized with the resized serverless app that is running along with the edits
+   * being applied
+   * @param key
+   * @param edits
+   */
+  public resizeWithInS3(key: string, edits: EditProperties) {
+
+    if (this._PARAM_LOCATION) {
+      key = `${this._PARAM_LOCATION}/${key}`;
+    }
+
+    const imageRequest = JSON.stringify({
+      bucket: "tomvisions-original-images",
+      key: key,
+      edits: edits
+    })
+
+    return `${Buffer.from(imageRequest).toString('base64')}`;
+  }
+
+  public setSitePrefix(prefix = true) {
+    if (!prefix) {
+      this._PARAM_LOCATION = null;
+      this._PARAM_LOCATION = null;
+    }
+  }
+}
+
+export interface EditProperties {
+  "resize": {
+    width?: number,
+    height?: number,
+    fit?: string
+  }
+}
